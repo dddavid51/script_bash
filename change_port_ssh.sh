1a04374e03fb6fe30cd3d7bc95a00b58851eaca0
dddavid51 <64151830+dddavid51@users.noreply.github.com>
2023-07-02 06:09:46 +0200

    Update change_port_ssh.sh
    Update version 1.0 to 1.1
    Update email

--- change_port_ssh.sh
+++ change_port_ssh.sh
@@ -1,18 +1,21 @@
 #! /usr/bin/env bash
- 
+
 # change_port_ssh.sh : Change ssh port for redhat enterprise server base.
-# Version: 1.0
+# Version: 1.1
 # Author: david.dzieciol
-# Email: david.dzieciol@gmail.com
-# Note: Tester sur rockylinux 9, almalinux 9, centos stream 9
- 
+# Email: david.dzieciol51100@gmail.com
+# Note: Test on rockylinux 9, almalinux 9, centos stream 9
+# Note: Add check whether the port to be replaced is already present in sshd_config
+
+
 # Check if executed as root user
-if [[ $EUID -ne 0 ]]; then
-        echo -e "This script has to be run as \033[1mroot\033[0m user."
-        exit 1
-fi
-           read -e -p "Please enter your new port number...? " new_port
- 
+           if [[ $EUID -ne 0 ]]; then
+           echo -e "This script has to be run as \033[1mroot\033[0m user."
+           exit 1
+           fi
+
+           read -e -p "Please enter your new ssh port number...? " new_port
+
            file_sshd_config="/etc/ssh/sshd_config"
            check_sshd_config=$(cat $file_sshd_config | grep -w Port | cut -d " " -f2) > /dev/null
            installdeps="dnf install -y policycoreutils"
@@ -22,10 +25,10 @@ fi
            firewall_remove="firewall-cmd --remove-port=$check_sshd_config/tcp --permanent"
            firewall_remove_service="firewall-cmd --remove-service=ssh --permanent"
            firewall_reload="firewall-cmd --reload"
- 
+
 function modify_port()
 {
-           if grep -w "#Port 22" $file_sshd_config > /dev/null
+           if grep -w "#Port 22" $file_sshd_config >/dev/null
            then
            echo "ssh is on port 22..."
            cp -f $file_sshd_config $file_sshd_config.orig.bak && cp -f $file_sshd_config /tmp/sshd_config.orig.bak
@@ -37,7 +40,11 @@ function modify_port()
               $firewall_remove_service
               $firewall_reload
            else
-           echo "ssh is not on port 22..."
+           if [[ $new_port = $check_sshd_config ]]; then
+           echo " port $new_port is already in sshd_config..."
+           exit 1
+           else
+           echo "old ssh port is $check_sshd_config replaced by port $new_port..."
            cp -f $file_sshd_config $file_sshd_config.bak && cp -f $file_sshd_config /tmp/sshd_config.bak
            sed -i "s/Port "$check_sshd_config"/Port "$new_port"/" "$file_sshd_config"
               $installdeps > /dev/null
@@ -46,9 +53,10 @@ function modify_port()
               $firewall_add
               $firewall_remove
               $firewall_reload
- 
+
+fi
 fi
 }
- 
+
 modify_port
 exit 0
